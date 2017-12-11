8c377e84e9170a7202dd4fdedb3a412eded9243e
Gonzalo Gluzman <dggluz@gmail.com>
2017-12-11 19:58:48 -0300

    feat(task.all): added Task.all static method
    * feat(task.all): added Task.all static method and its tests
    
    * fix(syntax): fixed trailing whitespaces
    
    * fix(test): Improved testing

--- src/task.ts
+++ src/task.ts
@@ -50,6 +50,51 @@ export class Task <T, E> {
         });
     }
 
+    static all <T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, E1, E2, E3, E4, E5, E6, E7, E8, E9, E10> (tasks: [Task<T1, E1>, Task<T2, E2>, Task<T3, E3>, Task<T4, E4>, Task<T5, E5>, Task<T6, E6>, Task<T7, E7>, Task<T8, E8>, Task<T9, E9>, Task<T10, E10>]): Task<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10], E1 | E2 | E3 | E4 | E5 | E6 | E7 | E8 | E9 | E10>;
+    static all <T1, T2, T3, T4, T5, T6, T7, T8, T9, E1, E2, E3, E4, E5, E6, E7, E8, E9> (tasks: [Task<T1, E1>, Task<T2, E2>, Task<T3, E3>, Task<T4, E4>, Task<T5, E5>, Task<T6, E6>, Task<T7, E7>, Task<T8, E8>, Task<T9, E9>]): Task<[T1, T2, T3, T4, T5, T6, T7, T8, T9], E1 | E2 | E3 | E4 | E5 | E6 | E7 | E8 | E9>;
+    static all <T1, T2, T3, T4, T5, T6, T7, T8, E1, E2, E3, E4, E5, E6, E7, E8> (tasks: [Task<T1, E1>, Task<T2, E2>, Task<T3, E3>, Task<T4, E4>, Task<T5, E5>, Task<T6, E6>, Task<T7, E7>, Task<T8, E8>]): Task<[T1, T2, T3, T4, T5, T6, T7, T8], E1 | E2 | E3 | E4 | E5 | E6 | E7 | E8>;
+    static all <T1, T2, T3, T4, T5, T6, T7, E1, E2, E3, E4, E5, E6, E7> (tasks: [Task<T1, E1>, Task<T2, E2>, Task<T3, E3>, Task<T4, E4>, Task<T5, E5>, Task<T6, E6>, Task<T7, E7>]): Task<[T1, T2, T3, T4, T5, T6, T7], E1 | E2 | E3 | E4 | E5 | E6 | E7>;
+    static all <T1, T2, T3, T4, T5, T6, E1, E2, E3, E4, E5, E6> (tasks: [Task<T1, E1>, Task<T2, E2>, Task<T3, E3>, Task<T4, E4>, Task<T5, E5>, Task<T6, E6>]): Task<[T1, T2, T3, T4, T5, T6], E1 | E2 | E3 | E4 | E5 | E6>;
+    static all <T1, T2, T3, T4, T5, E1, E2, E3, E4, E5> (tasks: [Task<T1, E1>, Task<T2, E2>, Task<T3, E3>, Task<T4, E4>, Task<T5, E5>]): Task<[T1, T2, T3, T4, T5], E1 | E2 | E3 | E4 | E5>;
+    static all <T1, T2, T3, T4, E1, E2, E3, E4> (tasks: [Task<T1, E1>, Task<T2, E2>, Task<T3, E3>, Task<T4, E4>]): Task<[T1, T2, T3, T4], E1 | E2 | E3 | E4>;
+    static all <T1, T2, T3, E1, E2, E3> (tasks: [Task<T1, E1>, Task<T2, E2>, Task<T3, E3>]): Task<[T1, T2, T3], E1 | E2 | E3>;
+    static all <T1, T2, E1, E2> (tasks: [Task<T1, E1>, Task<T2, E2>]): Task<[T1, T2], E1 | E2>;
+    static all <T1, E1> (tasks: [Task<T1, E1>]): Task<[T1], E1>;
+    static all <T, E> (tasks: Task<T, E>[]) {
+        // Flag to track if any Task has resolved
+        let rejected = false;
+        // Array that we'll fill with the resolved values, in order
+        const resolvedValues: T[] = [];
+        // Counter of resolved Tasks (we can't use resolvedValues.length since we add elements through index)
+        let resolvedQty = 0;
+
+        return new Task((outerResolve, outerReject) => {
+            tasks.forEach((aTask, index) => {
+                aTask
+                    .fork(err => {
+                        // We do only reject if there was no previous rejection
+                        if (!rejected) {
+                            rejected = true;
+                            outerReject(err);
+                        }
+                    }, x => {
+                        // Shouldn't resolve if another Task has rejected
+                        if (rejected) {
+                            return;
+                        }
+
+                        // Track resolved value (in order)
+                        resolvedValues[index] = x;
+                        // ...and how many tasks has resolved
+                        resolvedQty++;
+                        if (resolvedQty === tasks.length) {
+                            outerResolve(resolvedValues);
+                        }
+                    });
+            });
+        });
+    }
+
     map<TResult> (fn: IMapFn<T, TResult>): Task<TResult, E | UncaughtError> {
         return new Task((outerResolve, outerReject) => {
             this.fork(
